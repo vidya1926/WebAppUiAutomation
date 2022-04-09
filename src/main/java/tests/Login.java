ff18991d0d08a52f99bf1d469017a5082c88f4cf
TestLeaf <babu@testleaf.com>
2022-04-09 13:04:35 +0530

    Updated to Edge

--- src/main/java/tests/Login.java
+++ src/main/java/tests/Login.java
@@ -3,6 +3,8 @@ package tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -14,8 +16,8 @@ public class Login{
 	@Test()
 	public void login() {
 		
-		WebDriverManager.chromedriver().setup();
-		ChromeDriver driver = new ChromeDriver();
+		WebDriverManager.edgedriver().setup();
+		EdgeDriver driver = new EdgeDriver();
 		driver.get("http://localhost:9002/");
 		driver.findElement(By.xpath("//span[contains(text(),'use your account')]/following-sibling::input[@type='username']")).sendKeys("babu@testleaf.com");
 		driver.findElement(By.xpath("//span[contains(text(),'use your account')]/following-sibling::input[@type='password']")).sendKeys("tester@123");
